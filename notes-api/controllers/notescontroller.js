9cae08eb9ed0e65245761b4ac0f09f1877e0348b
kaushik-teksystems <kaushik.teksystems@gmail.com>
2026-04-01 16:17:59 +0530

    added update feature, and additional validations along with code coverage feature with c8

--- notes-api/controllers/notescontroller.js
+++ notes-api/controllers/notescontroller.js
@@ -1,6 +1,6 @@
-const {getNotes, saveNotes} = require('../services/notesService')
+const { getNotes, saveNotes } = require('../services/notesService')
 
-exports.getAllNotes = async(request, response) => {
+exports.getAllNotes = async (request, response) => {
     const notes = await getNotes();
 
     response.json(notes);
@@ -11,22 +11,24 @@ exports.getNoteById = async (request, response) => {
     const notes = await getNotes();
     const note = notes.find(n => n.id == request.params.id);
 
-    if(!note) response.status(404);
+    if (!note) response.status(404);
 
     response.json(note);
 };
 
 exports.createNote = async (request, response) => {
-    const {title, content} = request.body;
+    const { title, content } = request.body;
 
-    if(!title || !content) return response.status(400).json({error: 'Title & contnt required'});
+    if (!title || !content) return response.status(400).json({ error: 'Title & contnt required' });
 
     const notes = await getNotes();
 
     const newNote = {
         id: Date.now(),
         title,
-        content
+        content,
+        status: "created",
+        createdAt: new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' })
     };
 
     notes.push(newNote);
@@ -39,10 +41,35 @@ exports.deleteNote = async (request, response) => {
     const notes = await getNotes();
 
     const note = notes.find(n => n.id == request.params.id);
-    if(!note) return response.status(404).end();
+    if (!note) return response.status(404).end();
 
     const filtered = notes.filter(n => n.id != request.params.id);
 
     await saveNotes(filtered);
-    response.json({message: 'Deleted'});
+    response.json({ message: 'Deleted' });
+};
+
+exports.updateNote = async (request, response) => {
+    if (request.body.hasOwnProperty("createdAt")) {
+        return response.status(400).json({
+            error: "createdAt is a read-only property, and can't be overwritten"
+        });
+    }
+
+    const notes = await getNotes();
+
+    const existingNote = notes.find(n => String(n.id) === String(request.params.id));
+    if (!existingNote) return response.status(404).end();
+
+    const currentStatus = (existingNote.status || "").toString().trim().toLowerCase();
+    if (currentStatus === 'closed') {
+        return response.status(400).json({ error: "Can't update a closed note, try adding a seprarate note." });
+    }
+
+    const updated = notes.map(n =>
+        n.id == request.params.id ? { ...n, ...request.body } : n
+    );
+
+    await saveNotes(updated);
+    response.json({ message: 'updated' });
 };
