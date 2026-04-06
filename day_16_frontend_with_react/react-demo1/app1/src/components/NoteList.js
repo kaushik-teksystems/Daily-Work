c1a395b215ae7864650329312b7f1f1e62ad2d86
kaushik-teksystems <kaushik.teksystems@gmail.com>
2026-04-06 13:19:32 +0530

    Added Time required feature

--- day_16_frontend_with_react/react-demo1/app1/src/components/NoteList.js
+++ day_16_frontend_with_react/react-demo1/app1/src/components/NoteList.js
@@ -1,10 +1,15 @@
 import NoteItem from "./NoteItem";
 
-function NoteList({ notes, deleteNote }) {
+function NoteList({ notes, deleteNote, toggleComplete }) {
   return (
     <ul>
       {notes.map((note) => (
-        <NoteItem key={note.id} note={note} deleteNote={deleteNote} />
+        <NoteItem 
+        key={note.id} 
+        note={note} 
+        deleteNote={deleteNote}
+        toggleComplete={toggleComplete}   
+      />
       ))}
     </ul>
   );
